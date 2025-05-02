25221b2b9b85c42439a5600c04d91b1cf0715c8f
Tamiriscoliveira <tami_coliveira@hotmail.com>
2025-05-01 21:51:14 -0300

    aulas de terraform

--- descomplicando-terraform/main.tf
+++ descomplicando-terraform/main.tf
@@ -15,8 +15,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+     ami           = var.image_id
+     instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
---
+++ descomplicando-terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "image_id"{
+     type = string
+     description = "O id da AMI usada" 
+}
+
